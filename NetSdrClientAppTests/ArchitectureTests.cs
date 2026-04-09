902babb6078c204c130e46f68f09b82306f88555
AntAndriy7 <113107685+AntAndriy7@users.noreply.github.com>
2026-04-09 12:25:38 +0300

    lab5: add arch tests and broken dependency

---
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -0,0 +1,53 @@
+﻿using NetArchTest.Rules;
+using NUnit.Framework;
+using System.Reflection;
+using NetSdrClientApp.Networking;
+
+namespace NetSdrClientAppTests
+{
+    public class ArchitectureTests
+    {
+        [Test]
+        public void Messages_Should_Not_Depend_On_Networking()
+        {
+            // Arrange
+            var result = Types.InAssembly(typeof(NetSdrClientApp.Messages.NetSdrMessageHelper).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Messages")
+                .ShouldNot()
+                .HaveDependencyOn("NetSdrClientApp.Networking")
+                .GetResult();
+
+            // Assert
+            Assert.That(result.IsSuccessful, Is.True);
+        }
+
+        [Test]
+        public void Networking_Should_Not_Depend_On_Messages()
+        {
+            // Arrange
+            var result = Types.InAssembly(typeof(NetSdrClientApp.Networking.ITcpClient).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Networking")
+                .ShouldNot()
+                .HaveDependencyOn("NetSdrClientApp.Messages")
+                .GetResult();
+
+            // Assert
+            Assert.That(result.IsSuccessful, Is.True);
+        }
+
+        [Test]
+        public void Classes_ShouldResideInCorrectNamespace()
+        {
+            // Arrange
+            var result = Types.InAssembly(typeof(NetSdrClientApp.NetSdrClient).Assembly)
+                .Should()
+                .ResideInNamespaceStartingWith("NetSdrClientApp")
+                .GetResult();
+
+            // Assert
+            Assert.That(result.IsSuccessful, Is.True);
+        }
+    }
+}
